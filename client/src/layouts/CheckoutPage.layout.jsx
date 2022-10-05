76fe7c5cec17dab2e6259ca7a83b0c4a37cfae4f
IsaacRaj <isaacraj890@gmail.com>
2022-10-05 19:38:26 +0530

    created react app and setted up routing system

---
+++ client/src/layouts/CheckoutPage.layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Navbar from '../components/Navbar/CheckoutNavbar'
+const CheckoutPageLayout =(Component) => (...props) => {
+  return (
+    <>
+      {/* <Navbar/> */}
+      <div className='container mx-auto px-4 lg:px-20'>
+        <Component {...props}/>
+      </div>
+    </>
+  )
+}
+
+export default CheckoutPageLayout
